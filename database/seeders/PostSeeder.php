66c43de61f5a192801d2bb9cc6413447f6a8432e
Ali ÇAĞLAR <ali.caglar@machinarium.co>
2022-03-19 16:01:30 +0300

    add Post migration and add seeder

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Generator as Faker;
+
+class PostSeeder extends Seeder
+{
+    public function run(Faker $faker)
+    {
+        DB::table('posts')->truncate();
+
+        $content = '<p>' . implode('</p><p>', $faker->paragraphs(4)) . '</p>';
+
+        $data = [
+            'title' => $faker->sentence,
+            'content' => $content,
+            'author_name' => $faker->firstName . ' ' . $faker->lastName
+        ];
+
+        DB::table('posts')->insert($data);
+    }
+}
